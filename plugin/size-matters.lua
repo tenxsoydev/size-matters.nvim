d4f90aeb35f9d23a8e0868a0f65a557f880813af
Turiiya <34311583+tobealive@users.noreply.github.com>
2022-05-31 21:53:37 +0200

    Fix version check

--- plugin/size-matters.lua
+++ plugin/size-matters.lua
@@ -1,9 +1,9 @@
-if not vim.fn.has("nvim-0.7.0") then
-  vim.api.nvim_err_writeln("size-matters requires nvim version 0.7 or above")
-  return
+if vim.fn.has("nvim-0.7.0") == 0 then
+	vim.api.nvim_err_writeln("size-matters requires nvim version 0.7 or above")
+	return
 end
 
 if vim.g.loaden_size_matters then
-  return
+	return
 end
 vim.g.loaded_size_matters = true
