f9e10927386551af680d58fde7ee1bb05f8508b3
Turiiya <34311583+tobealive@users.noreply.github.com>
2022-12-08 14:21:47 +0100

    add tests

---
+++ tests/minimal_init.lua
@@ -0,0 +1,11 @@
+local plenary_dir = os.getenv "PLENARY_DIR" or "/tmp/plenary.nvim"
+if vim.fn.isdirectory(os.getenv "PLENARY_DIR" or "/tmp/plenary.nvim") == 0 then
+	vim.fn.system { "git", "clone", "https://github.com/nvim-lua/plenary.nvim", plenary_dir }
+end
+
+vim.opt.rtp:append "../"
+vim.opt.rtp:append(plenary_dir)
+vim.opt.guifont = "SomeNerdFont:h12.5"
+
+vim.cmd "runtime plugin/plenary.vim"
+require "plenary.busted"
