751f52ead4d731c3621a8585195be45a26cfcbe1
Turiiya <34311583+tobealive@users.noreply.github.com>
2023-02-04 21:08:52 +0100

    feat: support other notification provider

--- lua/size-matters/notifications.lua
+++ lua/size-matters/notifications.lua
@@ -1,22 +1,36 @@
 local M = {}
 
-local ok, notify = pcall(require, "notify")
-if not ok then return end
-
-local active_notification_win_id = -1
+local notifier_ok = pcall(require, "notifier")
+local notify_ok, notify = pcall(require, "notify")
+local active_notify_msg_win_id = -1
 
 ---@param message string
 ---@param config NotificationOpts
 function M.send(message, config)
+	if not notify_ok then
+		vim.defer_fn(function()
+			if notifier_ok then
+				local sm_notifications = 0
+				local notifier_status = require "notifier.status"
+				for _, notification in ipairs(notifier_status.active.nvim) do
+					if notification.mandat:match "FontSize" then sm_notifications = sm_notifications + 1 end
+				end
+				if sm_notifications > 1 then notifier_status.pop "nvim" end
+			end
+			vim.notify(message)
+		end, config.delay)
+		return
+	end
+
 	local notifyOpts = {
 		render = "minimal",
 		timeout = config.timeout,
 		minimum_width = 10,
 		on_open = function(win)
-			if vim.api.nvim_win_is_valid(active_notification_win_id) then
-				vim.api.nvim_win_close(active_notification_win_id, true)
+			if vim.api.nvim_win_is_valid(active_notify_msg_win_id) then
+				vim.api.nvim_win_close(active_notify_msg_win_id, true)
 			end
-			active_notification_win_id = win
+			active_notify_msg_win_id = win
 		end,
 	}
 
